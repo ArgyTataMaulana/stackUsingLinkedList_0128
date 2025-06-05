d6ca947131741b43c43b3b423322e339621e43af
ArgyTataMaulana <halmaheru37@gmail.com>
2025-06-05 14:58:23 +0700

    set the next pointer of thenewnode tothe current top node

--- stack.cpp
+++ stack.cpp
@@ -31,6 +31,7 @@ class Stack
         {
             Node *newNode =new Node(); // 1. allocate memory for the new node 
             newNode->data = value;     // 2. assign the value
+            newNode->next = top;       // 3. set the next pointer of the new node to the current top node
             
         }
 
