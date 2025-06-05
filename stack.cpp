068db8bcebde41e3e26daf6a30d83cb5a019e6d1
ArgyTataMaulana <halmaheru37@gmail.com>
2025-06-05 14:59:09 +0700

    update the top pointer ke node baru

--- stack.cpp
+++ stack.cpp
@@ -32,6 +32,7 @@ class Stack
             Node *newNode =new Node(); // 1. allocate memory for the new node 
             newNode->data = value;     // 2. assign the value
             newNode->next = top;       // 3. set the next pointer of the new node to the current top node
+            top = newNode;             // 4. Update the top pointer to the new node
             
         }
 
