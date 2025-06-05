b541a56b6e6190b65ce9adecc24d628575b13652
ArgyTataMaulana <halmaheru37@gmail.com>
2025-06-05 15:28:00 +0700

    Menambahkan kasus untuk keluar dari program dan kasus default untuk pilihan yang tidak valid dalam fungsi utama.

--- stack.cpp
+++ stack.cpp
@@ -112,7 +112,16 @@ int main()
         stack.peek();
         break;
 
+        case 4:
+        cout << "Exiting the program." << endl;
+        break;
+    default:
+    cout << "Invalid choice. Try again.." << endl;
+    break;
         }
+        cout << endl;
     }
+
+    return 0;    
     
 }
